2f431fd5e040c6fe4d8da11f38690c870d907b2e
balaram <balaramreddy1710@gmail.com>
2025-04-04 17:03:38 +0530

    [Balaram] Add: string compression

---
+++ Day-08/stringComp.js
@@ -0,0 +1,15 @@
+function compressString(str) {
+  let result = "";
+  let count = 1;
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++;
+    } else {
+      result += str[i - 1] + count;
+      count = 1;
+    }
+  }
+  return result.length < str.length ? result : str;
+}
+
+console.log(compressString("aabbbccccdd"));
